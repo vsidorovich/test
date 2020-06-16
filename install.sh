ca9f2f0ef42304aef9155e0e815f0bf46b28f39f
Vlad <v.v.sidorovich@gmail.com>
2020-06-16 12:01:22 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo  'test';
+
+run_composer() {
+  docker run --rm --interactive --tty --volume ./:/app $1
+}
+
+
+MAGENTO_DIR="magento"
+MAGENTO_VER="$1"
+if [ -z "$MAGENTO_VER" ]; then
+  MAGENTO_INSTALL="magento/project-community-edition"
+else
+  MAGENTO_INSTALL="magento/project-community-edition=" . $MAGENTO_VER
+fi
+
+run_composer composer create-project --repository-url=https://repo.magento.com/ $MAGENTO_INSTALL .
+
